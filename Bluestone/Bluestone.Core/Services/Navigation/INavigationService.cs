4bfae30690ef917b87e879e1d663233337bb1763
Kamaljit Singh <kamaljitdhadly@gmail.com>
2022-02-23 13:38:58 +0530

    Services file added

--- Bluestone/Bluestone.Core/Services/Navigation/INavigationService.cs
+++ Bluestone/Bluestone.Core/Services/Navigation/INavigationService.cs
@@ -1,11 +1,13 @@
-﻿using Bluestone.Core.ViewModels;
-using System;
+﻿using Bluestone.Core.ViewModels.Base;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Bluestone.Core.Services.Navigation
 {
     public interface INavigationService
     {
+        Task InitializeAsync();
 
+        Task NavigateToAsync (string route, IDictionary<string, string> routeParameters = null);
     }
 }
