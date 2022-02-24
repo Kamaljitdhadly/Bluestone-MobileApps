26544e559e72ddf0204187e62a44ee86960e5741
Kamaljit Singh <kamaljitdhadly@gmail.com>
2022-02-24 16:03:45 +0530

    Assemblyinfo updated

--- Bluestone/Bluestone.Core/AppLayout/Settings/AppSettings.cs
+++ Bluestone/Bluestone.Core/AppLayout/Settings/AppSettings.cs
@@ -19,6 +19,8 @@ namespace Bluestone.Core.AppLayout.Settings
 
         private bool isGridView;
 
+        public const string ApiUrlBase = "https://admin.joinme.co.in/";
+
         static AppSettings()
         {
             Instance = new AppSettings();
